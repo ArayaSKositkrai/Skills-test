95e033e539f4ce524fec0bf0099800383dbaf6d3
ArayaKositkrai <arayakositkrai@gmail.com>
2025-02-24 21:55:17 +0700

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
